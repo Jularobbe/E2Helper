b26994b07d7bcf689157fd1b0070aae504d7a8d2
Julian Friedrich <4139237+Jularobbe@users.noreply.github.com>
2025-01-24 22:41:29 +0000

    Fixes to pyinstaller

--- states/state.py
+++ states/state.py
@@ -1,16 +1,13 @@
 import os
 import sys
 
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.abspath(".")
+
 class State:
     def __init__(self, state_machine):
-        # Determine the base path for resources
-        if getattr(sys, 'frozen', False):
-            base_path = sys._MEIPASS
-        else:
-            base_path = os.path.abspath(".")
-
-        sys.path.append(base_path)
-        
         self.state_machine = state_machine
         self.banner_path = os.path.join(base_path, "resources", "banner.txt")
         self.storage_path = os.path.join(base_path, "resources", "storage.txt")
